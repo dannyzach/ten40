6421a934c57e6179d859e4e93e5e338002655da5
Danny Zach <dnyzch@gmail.com>
2024-12-18 08:41:29 +0200

    Update next.config.js. Lat functioning version.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   images: process.env.ENABLE_IMAGE_OPTIMIZATION === 'true' ? {
     domains: ['localhost'],
   } : {},
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3456/api/:path*'
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig; 
